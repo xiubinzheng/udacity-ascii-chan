eb4d954ab93aaf2adcfc1ab093e23e0d4aed70b6
xiubinzheng <xiubin.zheng7@gmail.com>
2016-12-28 20:29:26 -0500

    initial commit

---
+++ asciichan.py
@@ -0,0 +1,47 @@
+import os
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+class Art(db.Model):
+    title = db.StringProperty(required=True)
+    art = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *args, **kws):
+        self.response.out.write(*args, **kws)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    def render(self, template, **kws):
+        self.write(self.render_str(template, **kws))
+
+class MainPage(Handler):
+    def render_front(self,title="",art="",error=""):
+        self.render("ascii-chan.html",title=title,art=art,error=error)
+
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        art = self.request.get("art")
+
+        if title and art:
+            a = Art(title = title, art = art)
+            a.put()
+
+            self.redirect("/")
+        else:
+            error = "title and art work are needed!"
+            self.render_front(error = error)
+
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
